55c1e7fe583a6b23ea89d0b92c13b196ba2fc0bc
zombie110year <zombie110year@outlook.com>
2019-03-03 00:01:44 +0800

    精简 debug.py

--- debug.py
+++ debug.py
@@ -1,14 +1,6 @@
 from pic2text import _parse_args, main
 # import the entry of your program
 
-a = ["tests/pic.png", "-o", "tests/text.txt",]
-b = ["tests/pic.png", "-o", "tests/text.txt", "--width", "125"]
-c = ["tests/pic.png", "-o", "tests/text.txt", "--wh", "0.8"]
-d = ["tests/pic.png", "-o", "tests/text.txt", "--map", "!@#$%^&*()_+"]
-
-
 if __name__ == "__main__":
-    # args = _parse_args(d)
-    # print(args)
 
     main()
